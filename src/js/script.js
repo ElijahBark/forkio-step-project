e94918cbcb2986d8d8b91a84f09c3d22b6851f0c
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-11-10 14:52:22 +0200

    added icons to hover buttons

--- src/js/script.js
+++ src/js/script.js
@@ -15,12 +15,14 @@ $(document).ready(function(){
     });
 
     $(".twitter_btn").hover(function () {
+            $(this).children("img").attr("src","img/twitter-icon-hover.png");
             $(this).css({
                 "background-color" : "#ffffff",
                 "color" : "#21a7d2"
             })
         },
         function () {
+            $(this).children("img").attr("src","img/twitter-icon.png");
             $(this).css({
                 "background-color" : "#21a7d2",
                 "color" : "#ffffff"
@@ -28,12 +30,14 @@ $(document).ready(function(){
         });
 
     $(".github_btn").hover(function () {
+            $(this).children("img").attr("src","img/gh-icon-hover.png");
             $(this).css({
                 "background-color" : "#273f4c",
                 "color" : "#fff"
             })
         },
         function () {
+            $(this).children("img").attr("src","img/gh-icon.png");
             $(this).css({
                 "background-color" : "#f3f3f3",
                 "color" : "#000"
@@ -41,12 +45,14 @@ $(document).ready(function(){
         });
 
     $(".facebook_btn").hover(function () {
+            $(this).children("img").attr("src","img/facebook-icon-hover.png");
             $(this).css({
                 "background-color" : "#ffffff",
                 "color" : "#385899"
             })
         },
         function () {
+            $(this).children("img").attr("src","img/facebook-icon.png");
             $(this).css({
                 "background-color" : "#385899",
                 "color" : "#ffffff"
